31ed9907e2c4be4e713e11d0ca3da67cb2f0d2c8
Juan Santos <jsantos68@student.rccd.edu>
2024-02-22 11:33:37 -0800

    Added buttons to direct to features

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,6 @@
 import React from "react";
+import FeatureButton from "./FeatureButton"; // Import FeatureButton component
+
 //Hero function component is the giant movie collage
 export default function Hero() {
   return (
@@ -9,6 +11,20 @@ export default function Hero() {
           <p>Track and save movies.</p>
           <p>Read what others are saying.</p>
         </div>
+      </div>
+      <div className="buttonContainer">
+        <FeatureButton
+          //icon="icon-eye"
+          text="Keep track of every film you've ever watched"
+        />
+        <FeatureButton
+          //icon="icon-heart"
+          text="Rate each film on a five star scale"
+        />
+        <FeatureButton
+          //icon="icon-heart"
+          text="Write and Share Reviews, and view others"
+        />
       </div>
     </div>
   );
